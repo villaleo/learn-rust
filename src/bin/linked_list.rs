fc00ef0ad47cd2ac7ca6bb97cc85ef23a2588c3f
Leo Villalobos <villaleobos@gmail.com>
2022-12-13 21:31:36 -0800

    Add documentation for linked_list

--- src/bin/linked_list.rs
+++ src/bin/linked_list.rs
@@ -1,13 +1,20 @@
 use std::fmt::{Display, Formatter};
 
+/// A linked list [Link].
+///
+/// Create a new linked list using [Link::new].
 #[derive(Clone)]
 enum Link<T> {
+    /// An empty link (used to represent null).
     None,
+    /// A linked list node with a `value` and a reference to the `next` node.
     Link { item: T, next: Box<Link<T>> },
 }
 
+/// An iterator over a linked list.
 #[derive(Clone)]
-struct Cursor<T> {
+struct LinkIterator<T> {
+    /// The current node we are iterating over.
     curr: Link<T>,
 }
 
@@ -24,16 +31,16 @@ impl<T> Display for Link<T> where T: Copy + Display {
 
 impl<T> IntoIterator for Link<T> where T: Copy {
     type Item = T;
-    type IntoIter = Cursor<T>;
+    type IntoIter = LinkIterator<T>;
 
     fn into_iter(self) -> Self::IntoIter {
-        Cursor {
+        LinkIterator {
             curr: self
         }
     }
 }
 
-impl<T> Iterator for Cursor<T> where T: Copy {
+impl<T> Iterator for LinkIterator<T> where T: Copy {
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
@@ -43,26 +50,33 @@ impl<T> Iterator for Cursor<T> where T: Copy {
                 let mut n = Box::new(Link::None);
                 std::mem::swap(next, &mut n);
                 self.curr = *n;
-                return Some(item);
+                Some(item)
             }
         };
-        return next;
+        next
     }
 }
 
 impl<T> Link<T> where T: Copy + PartialEq {
+    /// Creates a new linked list.
     pub fn new() -> Self {
         Self::None
     }
 
-    pub fn push(&mut self, item: T) {
+    /// Pushes an item to the back of the list.
+    ///
+    /// The entire list must be traversed to insert so, the operation is O(n) time.
+    pub fn push_back(&mut self, item: T) {
         match self {
             Self::None => self.as_link(item),
-            Self::Link { next, .. } => next.push(item),
+            Self::Link { next, .. } => next.push_back(item),
         }
     }
 
-    pub fn pop(&mut self) -> Option<T> {
+    /// Pops the first item from the list and returns it, wrapped in an [Option]. If the list is empty, [None] is returned.
+    ///
+    /// `self` is a reference to the first item in the list so, the operation is O(1) time.
+    pub fn pop_front(&mut self) -> Option<T> {
         match self {
             Self::None => None,
             Self::Link { item, next } => {
@@ -71,11 +85,14 @@ impl<T> Link<T> where T: Copy + PartialEq {
 
                 std::mem::swap(next, &mut temp);
                 self.as_next(*temp);
-                return Some(item);
+                Some(item)
             }
         }
     }
 
+    /// Returns `true` if the item is in the list. `false` otherwise.
+    ///
+    /// The entire list will be traversed to find `item`, so the operation is O(n) time.
     pub fn contains(&self, item: T) -> bool {
         for node in self.clone() {
             if node == item {
@@ -85,6 +102,7 @@ impl<T> Link<T> where T: Copy + PartialEq {
         return false;
     }
 
+    /// Transforms `self` to a [Link::Link]. Panics if `self` is already a [Link::Link].
     fn as_link(&mut self, it: T) {
         *self = match self {
             Self::None => {
@@ -93,10 +111,11 @@ impl<T> Link<T> where T: Copy + PartialEq {
                     next: Box::new(Self::None),
                 }
             }
-            _ => panic!("Illegal state: Cannot convert to Link")
+            Self::Link { .. } => panic!("Illegal state: Cannot convert to Link")
         }
     }
 
+    /// Transforms `self` to the [Link] supplied.
     fn as_next(&mut self, next: Link<T>) {
         *self = next;
     }
@@ -104,14 +123,14 @@ impl<T> Link<T> where T: Copy + PartialEq {
 
 fn main() {
     let mut list = Link::<i32>::new();
-    list.push(10);
-    list.push(20);
-    list.push(30);
-    list.push(40);
-    list.push(50);
+    list.push_back(10);
+    list.push_back(20);
+    list.push_back(30);
+    list.push_back(40);
+    list.push_back(50);
 
     println!("List: {}", list);
-    println!("After popping off {}:\n{}", list.pop().unwrap(), list);
+    println!("After popping off {}:\n{}", list.pop_front().unwrap(), list);
     println!("List contains 40: {}", list.contains(40));
     println!("List contains 10: {}", list.contains(10));
 }
